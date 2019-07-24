3f18328397f2518a374854fa00f941036ef5c692
qi.wang01 <qi.wang01@weimob.com>
2019-07-24 17:55:34 +0800

    xss-反射型-演示

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.set('X-XSS-Protection', 0); // 关闭浏览器对xss的拦截
+  res.render('index', { title: 'Express', xss: req.query.xss });
 });
 
 module.exports = router;
