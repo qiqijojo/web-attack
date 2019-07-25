cbd791678fe1d1e5688fcabe6383a029c86a1de9
qi.wang01 <qi.wang01@weimob.com>
2019-07-25 10:07:41 +0800

    添加-评价功能的html、提交、获取评价、路由

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
+var comments = ''; // 缓存
+function html_encode(str) {
+  var s = '';
+  if(str.length === 0) return '';
+  s = str.replace(/&/g, '&gt;');
+  s = s.replace(/</g, '&lt;');
+  s = s.replace(/>/g, '&gt;');
+  s = s.replace(/\s/g, '&nbsp;');
+  s = s.replace(/\'/g, '&#39;');
+  s = s.replace(/\"/g,'&quot;');
+  s = s.replace(/\n/g, '<br>');
+  return s;
+};
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+// 构造提交评论接口
+router.get('/comment', function(req, res, next) {
+  comments.v = html_encode(req.query.comment);
+});
+
+// 获取评论接口
+router.get('/getComment', function(req, res, next) {
+  // 这里省略用户验证，直接将数据打出去
+  res.json({
+    comment: comments.v
+  })
+})
 
 module.exports = router;
